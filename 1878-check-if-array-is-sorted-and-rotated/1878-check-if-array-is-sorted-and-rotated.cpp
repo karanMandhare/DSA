e743c21a1995f0c6384bbb6b977a1498ec2e85bd
Karan Mandhare <hshhh496@gmail.com>
2025-11-12 23:23:07 +0530

    Time: 0 ms (100%), Space: 11.2 MB (75.11%) - LeetHub

--- 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.cpp
@@ -1,22 +1,35 @@
 class Solution {
 public:
     bool check(vector<int>& nums) {
-        int n = nums.size();
-        if(n == 1) return true;
-
-        int pos = 1;
-        while(pos < n && nums[pos] >= nums[pos - 1]) {
+        int n=nums.size();
+        if(n==1){
+        return true;
+        }
+        int pos=1;
+        //checking the first curve 
+        while(pos<n and nums[pos]>=nums[pos-1]){
             pos++;
         }
-
-        if(pos == n) return true;
-
-        // Check if the rest is sorted and wraps correctly
-        for(int i = pos + 1; i < n; i++) {
-            if(nums[i] < nums[i - 1]) return false;
+        //here we checked that the pos has reached upto N position(size) then we  return will true
+        if(pos==n)
+    return true;
+    //here if the problem has only one section so pos has reached upto size then return true
+    if(pos==n-1){
+        return nums[n-1]<=nums[0];
+    }
+        //else we will increased the position 
+        else {
+            pos++;
         }
+        
+        //here in 2nd curve check if 2nd pos is greater than frist half  as we know that pos of 2nd curve should be less than 1st graph 
+    if(pos>= n and nums[pos]>nums[0])
+    return false;
+  
+    //for checking the 2nd curve
+    while(pos<n and nums[pos]<=nums[0] and nums[pos]>=nums[pos-1] )
+  pos++;  
 
-        // Final wrap check
-        return nums[n - 1] <= nums[0];
+  return pos==n;
     }
 };
