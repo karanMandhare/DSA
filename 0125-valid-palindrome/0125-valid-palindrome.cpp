f2b4172ccf38dbdb6cd846dce5db0c7d1527f9bc
Karan Mandhare <hshhh496@gmail.com>
2025-11-17 22:06:57 +0530

    Time: 4 ms (17.57%), Space: 10.8 MB (5.76%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,45 @@
+class Solution {
+private:
+    bool valid(char ch) {
+        // Return true if character is alphanumeric
+        if ((ch >= 'a' && ch <= 'z') || 
+            (ch >= 'A' && ch <= 'Z') || 
+            (ch >= '0' && ch <= '9')) {
+            return true;
+        }
+        return false;
+    }
+
+    char tolowercase(char ch) {
+        // Convert uppercase to lowercase
+        if (ch >= 'A' && ch <= 'Z') {
+            return ch - 'A' + 'a';
+        }
+        return ch;
+    }
+
+    bool checkpalindrome(string a) {
+        int s = 0;
+        int e = a.length() - 1;
+        while (s <= e) {
+            if (a[s] != a[e]) {
+                return false;
+            } else {
+                s++;
+                e--;
+            }
+        }
+        return true;
+    }
+
+public:
+    bool isPalindrome(string s) {
+        string temp = "";
+        for (int j = 0; j < s.length(); j++) {
+            if (valid(s[j])) {
+                temp.push_back(tolowercase(s[j]));
+            }
+        }
+        return checkpalindrome(temp);
+    }
+};
